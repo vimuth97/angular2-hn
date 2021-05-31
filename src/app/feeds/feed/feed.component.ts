f20ca507816e60df443964b5811bc22d67533b70
SahanJayasinghe <47143531+SahanJayasinghe@users.noreply.github.com>
2021-05-31 15:30:00 +0530

    Pagination and auto news feed update implemented

--- src/app/feeds/feed/feed.component.ts
+++ src/app/feeds/feed/feed.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Subscription } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { HackerNewsAPIService } from '../../shared/services/hackernews-api.service';
 import { Story } from '../../shared/models/story';
@@ -12,18 +11,26 @@ import { Story } from '../../shared/models/story';
   styleUrls: ['./feed.component.scss']
 })
 
-export class FeedComponent implements OnInit {
+export class FeedComponent implements OnInit, OnDestroy {
   typeSub: Subscription;
   pageSub: Subscription;
+  intervalObserver: Subscription;
   items: Story[];
   feedType: string;
   pageNum: number;
   listStart: number;
   errorMessage = '';
+  validPageNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+  maxNewsPages = 10;
+  maxOtherPages = 2;
+  showToast: boolean = false;
+  newItems: Story[];
+  updateInterval: number = 120;  // seconds
 
   constructor(
     private _hackerNewsAPIService: HackerNewsAPIService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -34,7 +41,23 @@ export class FeedComponent implements OnInit {
       });
 
     this.pageSub = this.route.params.subscribe(params => {
+
+      if(Number.isInteger(+params['page']) && +params['page'] > 0) {
+        if (['news', 'newest'].includes(this.feedType) && +params['page'] > this.maxNewsPages) {
+          this.router.navigate([`/${this.feedType}`, `${this.maxNewsPages}`], {replaceUrl: true});
+        }
+        else if (['show', 'ask', 'jobs'].includes(this.feedType) && +params['page'] > this.maxOtherPages) {
+          this.router.navigate([`/${this.feedType}`, `${this.maxOtherPages}`], {replaceUrl: true});
+        }
+      }
+      else {
+        this.router.navigate([`/${this.feedType}`, '1'], {replaceUrl: true});
+      }
+      // if (!this.validPageNumbers.includes(params['page'])){
+      //   this.router.navigate([`/${this.feedType}`, '1'], {replaceUrl: true});
+      // }
       this.pageNum = params['page'] ? +params['page'] : 1;
+      console.log(`page: ${params['page']}`)
       this._hackerNewsAPIService.fetchFeed(this.feedType, this.pageNum)
         .subscribe(
           items => this.items = items,
@@ -45,5 +68,42 @@ export class FeedComponent implements OnInit {
           }
         );
     });
+
+    if (this.intervalObserver) this.intervalObserver.unsubscribe();
+
+    this.intervalObserver = interval(this.updateInterval*1000).subscribe(number => {
+      console.log(`Timer: ${number*2} mins`);
+      this._hackerNewsAPIService.fetchFeed(this.feedType, this.pageNum)
+        .subscribe(
+          items => {
+            if (items[0].id !== this.items[0].id) {
+              this.newItems = items;
+              console.log("New Feed Available!");
+              this.showToast = true;
+            }
+            // this.showToast = true;
+          },
+          error => this.errorMessage = 'Could not load ' + this.feedType + ' stories.',
+          () => {
+            this.listStart = ((this.pageNum - 1) * 30) + 1;
+            // window.scrollTo(0, 0);
+          }
+        );
+    })
+  }
+
+  goToPage(newPageNumber: number) {
+    // console.log(`this.pageNum: ${this.pageNum}`);
+    console.log(newPageNumber);
+    this.router.navigate([`/${this.feedType}`, `${newPageNumber}`]);
+  }
+
+  loadNewItems() {
+    this.items = this.newItems;
+    this.showToast = false;
+  }
+
+  ngOnDestroy(): void {
+    if (!this.intervalObserver.closed) this.intervalObserver.unsubscribe();
   }
 }
